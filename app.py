22774aab5f158f737a65d2f6a90f9c899081b016
FredrickMburu <109967236+FredrickMburu@users.noreply.github.com>
2025-03-21 00:12:03 +0300

    minor changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 CORS(app)  # Allow all origins
@@ -29,4 +30,5 @@ def predict():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
