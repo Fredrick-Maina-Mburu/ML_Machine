fcf848265d87c00c022824d699d0220d834f9936
FredrickMburu <109967236+FredrickMburu@users.noreply.github.com>
2025-03-20 23:15:46 +0300

    all changes complete

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import joblib
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)  # Allow all origins
+
 model = joblib.load(open('model.pkl', 'rb'))
 
 @app.route('/')
@@ -11,17 +14,15 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get data from form
     data = request.json
     try:
-        features = [
+        features = np.array([[
             int(data['Age']),
             int(data['Gender']),
             int(data['Polyuria']),
             int(data['Polydipsia']),
             int(data['sudden_weight_loss']),
-            
-        ]
+        ]])
         prediction = model.predict([features])[0]
         return jsonify({'prediction': 'Diabetic' if prediction == 1 else 'Not Diabetic'})
     except Exception as e:
